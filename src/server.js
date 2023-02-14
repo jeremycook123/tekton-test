b646c03994b56ca31eb17a01b85d0d7fa36af039
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-02-15 10:35:33 +1300

    minor updates

--- src/server.js
+++ src/server.js
@@ -2,17 +2,19 @@
 
 const express = require('express');
 
-// Constants
 const PORT = 8081;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/version', (req, res) => {
+  res.send('v1.0.0');
 });
 
 app.listen(PORT, HOST);
